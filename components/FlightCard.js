1ee122c162520786c318efbaa300dbdced30cf78
syed aadam ahmad <98835147+syedaadamahmad@users.noreply.github.com>
2025-11-14 17:27:52 +0530

    Update API URL to use environment variable

--- components/FlightCard.js
+++ components/FlightCard.js
@@ -75,7 +75,7 @@ export default function FlightCard({ flightData, bookingOptions }) {
   setIsLoading(true);
 
   try {
-    const res = await fetch('http://localhost:8000/chat/flight', {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/chat/flight`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
